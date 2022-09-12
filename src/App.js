5787417ddc6eccd96a12bb5b3858318a0829449d
Tran <ready0110@gmail.com>
2022-09-12 21:43:05 +0700

    change car color

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import HomeComponent from "./components/BTComponent/HomeComponent/HomeComponent"
 import DataBinding from "./components/DataBinding/DataBinding";
 import HandleEvent from "./components/HandleEvent/HandleEvent";
 import DemoState from "./DemoState/DemoState";
+import TangGiamFont from "./DemoState/TangGiamFont";
+import DemoStyling from "./DemoState/DemoStyling";
+import DoiMauXe from "./DemoState/DoiMauXe/DoiMauXe";
+import DemoArray from "./components/DemoArray/DemoArray";
 
 function App() {
   return (
     <div className="App">
-      <DemoState/>
+      <DemoArray/>
+      {/* <DoiMauXe/> */}
+      {/* <TangGiamFont/> */}
+      {/* <p className="demoCSS">DemoCSS</p>
+      <DemoStyling/> */}
+      {/* <DemoState/> */}
       {/* <HandleEvent /> */}
       {/* <DataBinding/> */}
       {/* component */}
